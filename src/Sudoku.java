e78c7215b82ed4384bd16daa1c30d757e06ed887
thomasdboer <t.d.boer@student.tue.nl>
2016-09-30 18:55:20 +0200

    Added Sudoku template

---
+++ src/Sudoku.java
@@ -0,0 +1,104 @@
+//Thomas den Boer, Thomas Willems
+//30-9-2016
+//This program solves a Sudoku
+class Sudoku {
+    private static final int SIZE = 9;     // size of the grid e.g. 9 -> 9x9
+    private static final int DMAX = 9;     // max digit to be filled in e.g. 9
+    private static final int BOXSIZE = 3;  // size of the boxes e.g. 3 -> 3x3
+    int[][] grid = new int[][] {
+            { 0, 6, 0,  0, 0, 1,  0, 9, 4 },
+            { 3, 0, 0,  0, 0, 7,  1, 0, 0 },
+            { 0, 0, 0,  0, 9, 0,  0, 0, 0 },
+            { 7, 0, 6,  5, 0, 0,  2, 0, 9 },
+            { 0, 3, 0,  0, 2, 0,  0, 6, 0 },
+            { 9, 0, 2,  0, 0, 6,  3, 0, 1 },
+            { 0, 0, 0,  0, 5, 0,  0, 0, 0 },
+            { 0, 0, 7,  3, 0, 0,  0, 0, 2 },
+            { 4, 1, 0,  7, 0, 0,  0, 8, 0 },
+    };
+
+    int solutionnr = 0; //solution counter
+
+    void run() {
+        //TODO starts the solving process.
+
+
+
+        //END TODO
+    }
+
+    boolean givesConflict(int r, int  c, int d) {
+        //TODO is there a conflict when we fill in d at position r,c?
+
+
+
+        //END TODO
+        return false;
+    }
+
+    boolean rowConflict(int r, int d) {
+        //TODO is there a conflict in row r when we fill in d?
+
+
+
+        //END TODO
+        return false;
+    }
+
+    boolean colConflict(int c, int d) {
+        //TODO is there a conflict in column c when we fill in d?
+
+
+
+        //END TODO
+        return false;
+    }
+
+    boolean boxConflict(int rr, int cc, int d) {
+        //TODO is there a conflict when we fill in d at position in the box of rr,cc?
+
+
+
+        //END TODO
+        return false;
+    }
+
+
+    int[] findEmptySquare() {
+        //TODO return the next empty square (See assignment).
+
+
+
+        //END TODO
+        return null;
+    }
+
+    void solve() {
+        //TODO see (4)
+
+
+
+        //END TODO
+    }
+
+    // print the grid, 0s are printed as spaces
+    void print() {
+        //TODO print the grid, printing spaces instead of 0s.
+
+
+
+        //END TODO
+    }
+
+    //TODO extra methods
+
+
+
+    //END TODO
+
+
+    public static void main(String[] args) {
+        new Sudoku().run();
+    }
+}
+0
